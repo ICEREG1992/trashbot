22d855d468c1c0558bdaa13b38c0e71a0fb47609
NicholasBrinkel <nickbrinkel@gmail.com>
2018-06-09 16:21:54 -0500

    HEAVILY Refactored and re-organized code. All different bot functionalities should now have their own file.

---
+++ src/src/StupidInstantHumor.java
@@ -0,0 +1,101 @@
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.event.message.MessageCreateEvent;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class StupidInstantHumor {
+
+    private static Map<String, String> keyPhrases = new HashMap<>();
+
+    private static File file;
+
+    public StupidInstantHumor(String filename) {
+        file = new File(filename);
+    }
+
+    public void run(MessageCreateEvent event) {
+        TextChannel channel = event.getChannel();
+        org.javacord.api.entity.message.Message message = event.getMessage();
+        String messageToString = message.getContent().toLowerCase();
+
+        for (String keyPhrase: keyPhrases.keySet()) {
+            if(messageToString.equals(keyPhrase)) {
+                if(messageToString.equals("/rule34")) {
+                    if ((int) (Math.random() * 4) == 1) {
+                        botWaitLong();
+                        channel.sendMessage("That's fucked.");
+                    }
+                } else {
+                    channel.sendMessage(keyPhrases.get(keyPhrase));
+                }
+            }
+        }
+    }
+
+    private static void botWaitLong() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println("bot's broke, boss");
+        }
+    }
+
+    public static void prepareStupidInstantHumorKeyPhrases() {
+        // This boolean is to keep track of whether the content in the destination file contains any information.
+        boolean hasAnyContent = true;
+        Scanner in = null;
+
+        // Creates a new scanner item to read from the file
+        try {
+            in = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found error: " + e);
+        }
+
+        // Reads in only the first line of the file
+        String key = in.nextLine();
+        String value = "";
+
+        // If the first line is empty or contains the escape sequence (***) then there is no content in the file.
+        if(key.equals("***") || key.equals("")) {
+            hasAnyContent = false;
+        }
+
+        // Loops until it reaches the escape sequece
+        while(hasAnyContent && !key.equals("***")) {
+            value = in.nextLine();
+            in.nextLine();
+            keyPhrases.put(key, value);
+            System.out.println(key);
+            System.out.println(value + "\n");
+            key = in.nextLine();
+        }
+        in.close();
+    }
+
+    // Adds a new keyword that Trashbot can respond to. The key is the keyword and the value is Trashbot's response.
+    public static void addKeyPhrasePair(String key, String value) {
+        PrintWriter out = null;
+        try {
+            out = new PrintWriter(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Error creating filewriter: " + e);
+        }
+
+        for (String oldKey: keyPhrases.keySet()) {
+            out.println(oldKey);
+            out.println(keyPhrases.get(oldKey) + "\n");
+        }
+
+        out.println(key);
+        out.println(value);
+        out.println("\n***");
+        keyPhrases.put(key, value);
+        out.close();
+    }
+}
