0dbbc8ec2a68e0f5920a704304824d1c4e37a520
icereg1992 <willwill.sullivan@gmail.com>
2021-06-23 00:29:03 -0400

    spam_train chance

--- train.py
+++ train.py
@@ -9,16 +9,34 @@ class spam_train:
             # get queue
             q = channels[message.channel.id]
             # add to queue/dequeue
+            q.append(message.content)
             if len(q) == 3:
-                q.pop(0)
-                q.append(message.content)
                 # check if all three the same
                 if (q[0] == q[1] and q[1] == q[2]):
-                    helperfunctions.bot_wait_long()
-                    await message.channel.send(message.content)
-            else:
-                q.append(message.content)
+                    # , afterwards smaller chance to add to chain
+                    if not q.active or (q.active and helperfunctions.chance(20)):
+                        helperfunctions.bot_wait()
+                        await message.channel.send(message.content)
+                    q.active = True
+                else:
+                    q.active = False
         else:
             # add channel to channels
-            channels[message.channel.id] = []
+            channels[message.channel.id] = Feed()
             channels[message.channel.id].append(message.content)
+
+class Feed:
+    def __init__(self, queue=[], active=False):
+        self.queue = queue
+        self.active = active
+
+    def __getitem__(self, key):
+        return self.queue[key]
+
+    def append(self, msg):
+        self.queue.append(msg)
+        if len(self) > 3:
+            self.queue.pop(0)
+
+    def __len__(self):
+        return len(self.queue)
