6aeb0dd1cdce636b35c98195445858dc84b53d80
William Sullivan <willwill.sullivan@gmail.com>
2022-07-08 01:50:20 -0500

    fix order of humor_escape

--- helperfunctions.py
+++ helperfunctions.py
@@ -29,13 +29,13 @@ def humor_escape(msg, text):
     else:
         text = text.replace("$n", msg.author.name)
 
-    if len(msg.mentions) > 0:
-        text = text.replace("$m", msg.mentions[0].mention)
-
     if len(msg.mentions) > 0 and hasattr(msg.mentions[0], 'nick') and msg.author.nick is not None:
         text = text.replace("$ms", msg.mentions[0].nick)
     elif len(msg.mentions) > 0:
         text = text.replace("$ms", msg.mentions[0].user)
+
+    if len(msg.mentions) > 0:
+        text = text.replace("$m", msg.mentions[0].mention)
     
     return text
 
