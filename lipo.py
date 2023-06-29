a5f00e23f48e2c2cf93338ddc20a3620b26a2e2e
William Sullivan <willwill.sullivan@gmail.com>
2023-06-29 02:35:03 -0500

    fix lipo uids

--- lipo.py
+++ lipo.py
@@ -21,19 +21,20 @@ class lipo:
         participants = json.loads(d['Item']['data']['S'])
 
     async def run(self, message):
+        uid = str(message.author.id)
         if (message.content.startswith("!lipo")):
             if ' ' not in message.content:
                 az = list(map(chr, range(97, 123)))
                 c = pick_string(az)
             elif len(message.content[6:]) >= 1:
                 c = message.content[6:].lower()
-            if message.author.id not in participants:
-                participants[message.author.id] = {}
-                participants[message.author.id]['c'] = c
-                participants[message.author.id]['points'] = 0
-                participants[message.author.id]['name'] = message.author.name
-                participants[message.author.id]['start'] = dt.datetime.utcnow().timestamp()
-                participants[message.author.id]['best'] = ""
+            if uid not in participants:
+                participants[uid] = {}
+                participants[uid]['c'] = c
+                participants[uid]['points'] = 0
+                participants[uid]['name'] = message.author.name
+                participants[uid]['start'] = dt.datetime.utcnow().timestamp()
+                participants[uid]['best'] = ""
                 if len(c) > 1:
                     await message.channel.send(message.author.mention + ", you have started a lipogram challenge for the letters in `" + c + "`. Have fun!")
                 else:
@@ -41,20 +42,20 @@ class lipo:
                 logcommand.log_globally(logging.INFO, "New lipo challenge started by " + message.author.name + " for letter `" + c + "`")
                 lipo.save()
             else:
-                c = participants[message.author.id]['c']
+                c = participants[uid]['c']
                 if len(c) > 1:
-                    await message.channel.send("you're doing a lipo challenge for the letters in `" + c + "` with `" + str(participants[message.author.id]['points']) + " points`")
+                    await message.channel.send("you're doing a lipo challenge for the letters in `" + c + "` with `" + str(participants[uid]['points']) + " points`")
                 else:
-                    await message.channel.send("you're doing a lipo challenge for letter `" + c + "` with `" + str(participants[message.author.id]['points']) + " points`")
+                    await message.channel.send("you're doing a lipo challenge for letter `" + c + "` with `" + str(participants[uid]['points']) + " points`")
         else:
             st = message.content.lower()
             # if re.fullmatch(r'https?:\/\/(?:www\.)?(?:(?:t(?:e|x)nor\.com\/vi(?:e|x)w\/)|(?:giphy\.com\/gifs\/)|(?:[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)\.(jpg|jpeg|png|gif|gifv|webm|mp4|mov)))(?:\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*))?', str): # should cover most image embed type that shows up on discord, doesnt match if the text is not shown (WILL MATCH 'message [link]' AND WHATNOT, WILL MATCH YT/TWITTER/SPOTIFY LINKS)
                 # return # idk feel free to reward points i dont think it should count lol
             st = re.sub(r'[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)', '', st) # erase all URLs
             st = re.sub(r'(<a?)?:\w+:(\d{18}>)?', '', st) # erase discord emojis
-            if message.author.id in participants:
+            if uid in participants:
                 matches = None
-                for n in participants[message.author.id]['c']:
+                for n in participants[uid]['c']:
                     if n in st:
                         # extract all of the character words found in the string
                         words = re.split('\W+', st)
@@ -64,20 +65,20 @@ class lipo:
                             matches[0] = n
                         break
                 if matches:
-                    await message.channel.send(pick_string(["epic fail (" + str(participants[message.author.id]['points']) + " points)",
-                    "lipogram challenge failed (" + str(participants[message.author.id]['points']) + " points)",
-                    "it was a good try but you failed after " + str(participants[message.author.id]['points']) + " messages",
-                    "no dice, i see a `" + matches[1] + "` in there (" + str(participants[message.author.id]['points']) + " points)",
-                    "sadly you're not allowed to say `" + pick_string(matches[0]) + "` (" + str(participants[message.author.id]['points']) + " points)",
-                    "lol this user said " + pick_string(matches[0]) + " (" + str(participants[message.author.id]['points']) + " points)",
-                    "laugh at this user they used letter " + matches[1] + " (" + str(participants[message.author.id]['points']) + " points)",
-                    "YOU CAN\"T SAY `" + pick_string(matches[0]) + "` LOLLLLLLL (" + str(participants[message.author.id]['points']) + " points)"]))
-                    logcommand.log_globally(logging.INFO, "`" + participants[message.author.id]['c'] + "` lipogram challenge for " + message.author.name + " has ended with `" + str(participants[message.author.id]['points']) + "` points.")
-                    participants.pop(message.author.id, None)
+                    await message.channel.send(pick_string(["epic fail (" + str(participants[uid]['points']) + " points)",
+                    "lipogram challenge failed (" + str(participants[uid]['points']) + " points)",
+                    "it was a good try but you failed after " + str(participants[uid]['points']) + " messages",
+                    "no dice, i see a `" + matches[1] + "` in there (" + str(participants[uid]['points']) + " points)",
+                    "sadly you're not allowed to say `" + pick_string(matches[0]) + "` (" + str(participants[uid]['points']) + " points)",
+                    "lol this user said " + pick_string(matches[0]) + " (" + str(participants[uid]['points']) + " points)",
+                    "laugh at this user they used letter " + matches[1] + " (" + str(participants[uid]['points']) + " points)",
+                    "YOU CAN\"T SAY `" + pick_string(matches[0]) + "` LOLLLLLLL (" + str(participants[uid]['points']) + " points)"]))
+                    logcommand.log_globally(logging.INFO, "`" + participants[uid]['c'] + "` lipogram challenge for " + message.author.name + " has ended with `" + str(participants[uid]['points']) + "` points.")
+                    participants.pop(uid, None)
                 else:
-                    participants[message.author.id]['points'] += 1
-                    if 'best' in participants[message.author.id] and len(st) > len(participants[message.author.id]['best']):
-                        participants[message.author.id]['best'] = st
+                    participants[uid]['points'] += 1
+                    if 'best' in participants[uid] and len(st) > len(participants[uid]['best']):
+                        participants[uid]['best'] = st
                 lipo.save()
                     
     def save():
