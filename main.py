2762b23848a7d850fa66ef305856d045615e563b
William Sullivan <willwill.sullivan@gmail.com>
2023-07-17 23:57:15 -0500

    disable lipo module

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from wordplay import wordplay
 from mcplayers import mcplayers
 from powerswitch import powerswitch
 from rdj import rdj
-from lipo import lipo
+# from lipo import lipo
 from qat import qat
 import logcommand, logging
 logging.basicConfig(level=logging.INFO)
@@ -45,7 +45,7 @@ class MyClient(discord.Client):
         # wordplay.init()
         uptime.init()
         food.init()
-        lipo.init()
+        # lipo.init()
         await powerswitch.init(self)
 
     async def on_message(self, message):
@@ -68,7 +68,7 @@ class MyClient(discord.Client):
                 await uptime.run(self, message)
                 await food.run(self, message, powerswitch)
                 await rdj.run(self, message)
-                await lipo.run(self, message)
+                # await lipo.run(self, message)
                 await qat.run(self, message)
                 # await wordplay.run(self, message)
                 # await karaoke_manager.run(self, message)
