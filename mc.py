f0d722bd06015badc8a694e47ea7ca34402187d0
William Sullivan <willwill.sullivan@gmail.com>
2023-10-15 01:36:53 -0500

    fix popen call

--- mc.py
+++ mc.py
@@ -59,7 +59,7 @@ class mc:
                     "\*inhales\*",
                     "ok one sec"
                 ]))
-                server = subprocess.Popen('java', '-Xmx1024M', '-Xms1024M', '-jar', '/home/william/minecraft/server.jar' ,'nogui', stdin=subprocess.PIPE)
+                server = subprocess.Popen('java', '-Xmx1024M', '-Xms1024M', '-jar', '/home/william/minecraft/server.jar' ,'nogui')
                 helperfunctions.bot_wait_long()
                 await message.channel.send(helperfunctions.pick_string([
                     "ok im runnin",
