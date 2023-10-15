378e42760fbc5d9395edb821d5b0b5c709b8005a
William Sullivan <willwill.sullivan@gmail.com>
2023-10-15 01:40:48 -0500

    newer mc path

--- mc.py
+++ mc.py
@@ -59,7 +59,7 @@ class mc:
                     "\*inhales\*",
                     "ok one sec"
                 ]))
-                server = subprocess.Popen(['java', '-Xmx1024M', '-Xms1024M', '-jar', '/home/william/minecraft/server.jar' ,'nogui'], stdin=subprocess.PIPE)
+                server = subprocess.Popen(['java', '-Xmx1024M', '-Xms1024M', '-jar', '/home/william/trashbot/server/server.jar' ,'nogui'], stdin=subprocess.PIPE)
                 helperfunctions.bot_wait_long()
                 await message.channel.send(helperfunctions.pick_string([
                     "ok im runnin",
@@ -75,6 +75,7 @@ class mc:
             
         elif message.content == "!stophost" and permissions.allowed(message.author.id, "blue"):
             server.kill()
+            server = None
 
     def save():
         global mcIP
