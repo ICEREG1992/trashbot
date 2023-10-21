94dd345f098bb22b6e8bac0da4a6e3e3808ceb5c
William Sullivan <willwill.sullivan@gmail.com>
2023-10-21 17:41:38 -0500

    change escape character

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ class MyClient(discord.Client):
                     await message.add_reaction("🇲")
 
                 if message.content == "!version":
-                    await message.channel.send("u last pushed to me §DATE")
+                    await message.channel.send("u last pushed to me •DATE")
 
                 if helperfunctions.chance(.002):
                     await message.channel.send("Error")
