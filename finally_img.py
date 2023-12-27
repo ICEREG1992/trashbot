f73bf7ce00af1211bbcbd7c9dd02de7ab15da503
William Sullivan <willwill.sullivan@gmail.com>
2023-12-27 03:07:45 -0600

    ratelimit finally command

--- finally_img.py
+++ finally_img.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import helperfunctions
 import logcommand, logging
+import datetime as dt
 import textwrap
 import io
 import base64
@@ -8,41 +9,47 @@ import discord
 import requests
 from bs4 import BeautifulSoup
 
+global t
+t = dt.datetime.utcnow()
+
 class finally_img:
 
     async def run(self, message):
         if (message.content.startswith("!finally ")):
-            template_img = Image.open("finally.png")
-            text = message.content[9:]
-            # add image
-            google_image = finally_img.get_google_image(text)
-            # open the image url pulled
-            google_image_request = requests.get(google_image)
-            if google_image_request.status_code == 200:
-                image_stream = io.BytesIO(google_image_request.content)
-                google_image = Image.open(image_stream)
+            if (t > dt.datetime.utcnow() - dt.timedelta(seconds=30)):
+                template_img = Image.open("finally.png")
+                text = message.content[9:]
+                # add image
+                google_image = finally_img.get_google_image(text)
+                # open the image url pulled
+                google_image_request = requests.get(google_image)
+                if google_image_request.status_code == 200:
+                    image_stream = io.BytesIO(google_image_request.content)
+                    google_image = Image.open(image_stream)
+                else:
+                    await channel.send("oops something went wrong back here")
+                    return
+                # paste image onto template
+                template_img.paste(google_image, (150,70))
+                # add text
+                shadowcolor = "black"
+                fillcolor = "white"
+                x = 100
+                y = 320
+                font = ImageFont.truetype('IMPACT.TTF', 46)
+                # no need for textwrap here
+                draw = ImageDraw.Draw(template_img)
+                # thicker border
+                text = text.upper()
+                draw.text((x-2, y-2), text, font=font, fill=shadowcolor)
+                draw.text((x+2, y-2), text, font=font, fill=shadowcolor)
+                draw.text((x-2, y+2), text, font=font, fill=shadowcolor)
+                draw.text((x+2, y+2), text, font=font, fill=shadowcolor)
+                # now draw the text over it
+                draw.text((x, y), text, font=font, fill=fillcolor)
+                await finally_img.send_image(template_img, message.channel)
             else:
-                await channel.send("oops something went wrong back here")
-                return
-            # paste image onto template
-            template_img.paste(google_image, (150,70))
-            # add text
-            shadowcolor = "black"
-            fillcolor = "white"
-            x = 100
-            y = 320
-            font = ImageFont.truetype('IMPACT.TTF', 46)
-            # no need for textwrap here
-            draw = ImageDraw.Draw(template_img)
-            # thicker border
-            text = text.upper()
-            draw.text((x-2, y-2), text, font=font, fill=shadowcolor)
-            draw.text((x+2, y-2), text, font=font, fill=shadowcolor)
-            draw.text((x-2, y+2), text, font=font, fill=shadowcolor)
-            draw.text((x+2, y+2), text, font=font, fill=shadowcolor)
-            # now draw the text over it
-            draw.text((x, y), text, font=font, fill=fillcolor)
-            await finally_img.send_image(template_img, message.channel)
+                await message.add_reaction('🚫')
 
     async def send_image(img, channel):
         with io.BytesIO() as out:
