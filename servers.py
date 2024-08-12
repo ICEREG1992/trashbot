860ecdce31471b4b005a3e81237117ebde91f66c
William Sullivan <willwill.sullivan@gmail.com>
2024-08-11 22:37:31 -0500

    switch mc to aikar's flags

--- servers.py
+++ servers.py
@@ -114,7 +114,7 @@ class servers:
                     "\*inhales\*",
                     "ok one sec"
                 ]))
-                server = subprocess.Popen(['java','-Xms1G','-Xmx1G','-jar','server.jar','nogui'], cwd=r'/home/william/minecraft/', stdin=subprocess.PIPE)
+                server = subprocess.Popen(['java', '-Xms1024M', '-Xmx1024M', '-XX:+AlwaysPreTouch', '-XX:+DisableExplicitGC', '-XX:+ParallelRefProcEnabled', '-XX:+PerfDisableSharedMem', '-XX:+UnlockExperimentalVMOptions', '-XX:+UseG1GC', '-XX:G1HeapRegionSize=8M', '-XX:G1HeapWastePercent=5', '-XX:G1MaxNewSizePercent=40', '-XX:G1MixedGCCountTarget=4', '-XX:G1MixedGCLiveThresholdPercent=90', '-XX:G1NewSizePercent=30', '-XX:G1RSetUpdatingPauseTimePercent=5', '-XX:G1ReservePercent=20', '-XX:InitiatingHeapOccupancyPercent=15', '-XX:MaxGCPauseMillis=200', '-XX:MaxTenuringThreshold=1', '-XX:SurvivorRatio=32','-jar','server.jar','nogui'], cwd=r'/home/william/minecraft/', stdin=subprocess.PIPE)
                 helperfunctions.bot_wait_long()
                 await message.channel.send(helperfunctions.pick_string([
                     "ok im runnin",
