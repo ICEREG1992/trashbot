af1a73f3714052ff84f3c527480bc30bb93194ec
noneuclidean <eb69305@gmail.com>
2025-01-17 00:24:56 -0800

    Update food.py
    comma

--- food.py
+++ food.py
@@ -66,7 +66,7 @@ class food:
                     "manna from heaven... ACK!!",
                     "IVE BEEN SO HUNGRY THANNK YOU",
                     "AAAAAAAAAAHHHHHHHHHH!!!!!!!!",
-                    "scrumptious (this message written by user noneuclidean)"
+                    "scrumptious (this message written by user noneuclidean)",
                     "im eating it im eating it im eating it im eating it im eating it im eating it im eating it im eating it"
                 ]))
             else:
