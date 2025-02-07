2d2576e45563c4b28ba0472cda460f6ef05484f8
William Sullivan <willwill.sullivan@gmail.com>
2025-02-06 22:27:02 -0600

    add !pan

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from powerswitch import powerswitch
 from numerology import numerology
 from rdj import rdj
 from lipo import lipo
+from pan import pan
 from qat import qat
 import logcommand, logging
 logging.basicConfig(level=logging.INFO)
@@ -50,6 +51,7 @@ class MyClient(discord.Client):
         uptime.init()
         food.init()
         lipo.init()
+        pan.init()
         await powerswitch.init(self)
 
     async def on_message(self, message):
@@ -75,6 +77,7 @@ class MyClient(discord.Client):
                 await rdj.run(self, message)
                 await finally_img.run(self, message)
                 await lipo.run(self, message)
+                await pan.run(self, message)
                 await qat.run(self, message)
                 await numerology.run(self, message)
                 # await wordplay.run(self, message)
---
+++ pan.py
@@ -0,0 +1,81 @@
+import json
+import discord
+import helperfunctions
+import humanize
+import datetime as dt
+from helperfunctions import pick_string
+from permissions import permissions
+from operator import attrgetter
+import boto3
+import logcommand, logging
+import re
+
+global db
+db = boto3.client('dynamodb', region_name='us-east-2')
+
+global participants
+participants = {}
+
+class pan:
+
+    def init():
+        d = db.get_item(TableName="trashbot", Key={'name':{'S':'pan'}})
+        global participants
+        participants = json.loads(d['Item']['data']['S'])
+
+    async def run(self, message):
+        global participants
+        uid = str(message.author.id)
+        if (message.content.startswith("!panboard")):
+            t = pick_string(["🏆 currently running pangram challenges:\n",
+            "🏆 get a load a these top " + (str(len(participants)) if len(participants) < 9 else "eight") + " nerds:\n",
+            "🏆 here's currently pangrams:\n",
+            "🏆 check check check check it out:\n",
+            "🏆 eight"])
+            embed = discord.Embed(title=t)
+            for l in sorted(participants.items(), key=lambda item: item[1]['points'], reverse=True)[0:8]:
+                embed.add_field(name=l[1]['name'], value=str(l[1]['points']) + " points for `" + (l[1]['c'] if len(l[1]['c']) <= 26 else (l[1]['c'][0:26] + "...")) + "` since <t:" + str(int(l[1]['start'])) + ">", inline=False)
+            await message.channel.send(embed=embed)
+        elif (message.content.startswith("!pan")):
+            if uid not in participants:
+                participants[uid] = {}
+                participants[uid]['points'] = 0
+                participants[uid]['name'] = message.author.name
+                participants[uid]['start'] = dt.datetime.now().timestamp()
+                participants[uid]['best'] = ""
+                await message.channel.send(message.author.mention + ", you have started a pangram challenge. every message you send must contain all letters of the alphabet. for more information, read a book.")
+                logcommand.log_globally(logging.INFO, "New pangram challenge started by " + message.author.name + " for letter `" + c + "`")
+                pan.save()
+            else:
+                await message.channel.send("you're doing a pangram challenge with `" + str(participants[uid]['points']) + " points`")
+        else:
+            st = message.content.lower()
+            if uid in participants:
+                if set(string.ascii_lowercase).issubset(set(s.lower())): # pangram check is here
+                    if (participants[uid]['points'] == 0):
+                        await message.reply(pick_string(["YOU DON'T HAVE TO DO A PANGRAM IF YOU DON'T WANT TO (" + str(participants[uid]['points']) + " points)",
+                        "THE JOKE ISN'T FUNNY. YOU LOSE. (" + str(participants[uid]['points']) + " points)",
+                        "DID YOU ENJOY YOURSELF? (" + str(participants[uid]['points']) + " points)",
+                        "HAHAHAHAHAHAHA GOOD ONE I LIKED THAT ONE SEE YOU STARTED A PANGRAM THEN IMMEDIATELY FAILED SO FUNNY (" + str(participants[uid]['points']) + " points)",
+                        "BRUH (" + str(participants[uid]['points']) + " points)"]))
+                    else:
+                        await message.reply(pick_string(["epic fail (" + str(participants[uid]['points']) + " points)",
+                        "pangram challenge failed (" + str(participants[uid]['points']) + " points)",
+                        "it was a good try but you failed after " + str(participants[uid]['points']) + " messages",
+                        "no dice, i see a `" + matches[1] + "` in there (" + str(participants[uid]['points']) + " points)",
+                        "sadly you're not allowed to say `" + pick_string(matches[0]) + "` (" + str(participants[uid]['points']) + " points)",
+                        "lol this user said " + pick_string(matches[0]) + " (" + str(participants[uid]['points']) + " points)",
+                        "laugh at this user they used letter " + matches[1] + " (" + str(participants[uid]['points']) + " points)",
+                        "YOU CAN\"T SAY `" + pick_string(matches[0]) + "` LOLLLLLLL (" + str(participants[uid]['points']) + " points)"]))
+                    logcommand.log_globally(logging.INFO, "`" + participants[uid]['c'] + "` pangram challenge for " + message.author.name + " has ended with `" + str(participants[uid]['points']) + "` points.")
+                    participants.pop(uid, None)
+                else:
+                    participants[uid]['points'] += 1
+                    if 'best' in participants[uid] and len(st) > len(participants[uid]['best']):
+                        participants[uid]['best'] = st
+                pan.save()
+                    
+    def save():
+        global participants
+        # we have to do shenanigans here to be able to serialize our dict with tuple keys
+        db.put_item(TableName="trashbot", Item={'name':{'S':'pan'}, 'data':{'S':json.dumps(participants)}})
