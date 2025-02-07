5b343094db994fb47e8f6164b05ea41ec9c8a518
William Sullivan <willwill.sullivan@gmail.com>
2025-02-06 21:51:18 -0600

    add blank responses

--- numerology.py
+++ numerology.py
@@ -6,3 +6,11 @@ class numerology:
             arr = list(' '.join(message.content.split(' ')[1:]).lower())
             total = sum(ord(c) - 97 for c in arr if 'a' <= c <= 'z')
             await message.channel.send(str(total))
+        elif message.content.startswith('!num'):
+            await message.channel.send(helperfunctions.pick_string([
+                'num num num',
+                'num num num num num',
+                'nom nom nom hehe',
+                'num num give me gum gum',
+                '🥶❄️ HELP ITS SO COLD IM TURNING NUMB PLS LET ME INSIDE 🧊😰☃️'
+            ]))
