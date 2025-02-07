db57ffdc06a34e705dea66be16d7d269a5a1e428
William Sullivan <willwill.sullivan@gmail.com>
2025-02-06 21:43:19 -0600

    remove keyword

--- numerology.py
+++ numerology.py
@@ -4,7 +4,7 @@ class numerology:
     async def run(self, message):
         if message.content.startswith('!num ') or message.content.startswith('!numerology '):
             arr = list(' '.join((message.content.split(' ')[1:]).lower()))
-            sum = 0
+            s = 0
             for c in arr:
-                sum += ord(c) - 97
-            await message.channel.send(str(sum))
+                s += ord(c) - 97
+            await message.channel.send(str(s))
