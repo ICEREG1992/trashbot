23c0d13346e7734805f3291d15a0b1d12c98719f
William Sullivan <willwill.sullivan@gmail.com>
2025-02-06 21:45:31 -0600

    replace with ai code because i suck

--- numerology.py
+++ numerology.py
@@ -2,9 +2,7 @@ import helperfunctions
 
 class numerology:
     async def run(self, message):
-        if message.content.startswith('!num ') or message.content.startswith('!numerology '):
-            arr = list(' '.join((message.content.split(' ')[1:]).lower()))
-            s = 0
-            for c in arr:
-                s += ord(c) - 97
-            await message.channel.send(str(s))
+        if message.content.startswith(('!num ', '!numerology ')):
+            arr = list(' '.join(message.content.split(' ')[1:]).lower())
+            total = sum(ord(c) - 97 for c in arr if 'a' <= c <= 'z')
+            await message.channel.send(str(total))
