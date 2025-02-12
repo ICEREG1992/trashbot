7d1fdb0efbfdb5b010ea1aaac5066c57dff95beb
William Sullivan <willwill.sullivan@gmail.com>
2025-02-11 22:45:31 -0600

    enable replies for !rot13

--- numerology.py
+++ numerology.py
@@ -34,18 +34,50 @@ class numerology:
         elif message.content.startswith("!rot"):
             spoilerResult = False
             args = message.content.split(' ')
-            n = int(args[0][4:])
+            try:
+                n = int(args[0][4:])
+            except NameError:
+                await message.channel.send(helperfunctions.pick_string([
+                    "don't see a rot value for me",
+                    "try specifying a number",
+                    "rot"
+                ]))
+                return
             msg = ' '.join(args[1:]).upper()
-            if msg.startswith("||") and msg.endswith("||"):
-                spoilerResult = True
-                msg = msg[2:-2] 
-            rot = [numerology.shift(c, n) for c in msg]
-            out = ''.join(rot)
-            if spoilerResult:
-                out = '||' + out + '||'
-            await message.channel.send(out)
+            if msg:
+                out = numerology.rot(msg, n)
+                await message.channel.send(out)
+            else:
+                # no message, check if this is a reply
+                if message.type == "reply":
+                    og = message.reference.resolved
+                    if og.embeds:
+                        await message.channel.send(og.embeds[0].description)
+                    else:
+                        out = numerology.rot(og.content, n)
+                        await message.channel.send(out)
+                else:
+                    await message.channel.send(helperfunctions.pick_string([
+                        "don't see anything to rot",
+                        "try specifying a string or reply",
+                        "rot"
+                    ]))
+
+    def rot(msg, n):
+        if msg.startswith("||") and msg.endswith("||"):
+            spoilerResult = True
+            msg = msg[2:-2] 
+        rot = [numerology.shift(c, n) for c in msg]
+        out = ''.join(rot)
+        if spoilerResult:
+            out = '||' + out + '||'
+        return out
+
 
     def shift(c, n):
         letter = ord(c) - 65
-        rot = (letter + n) % 26
-        return chr(rot + 65)
+        if 0 <= letter < 26:
+            rot = (letter + n) % 26
+            return chr(rot + 65)
+        else:
+            return c
