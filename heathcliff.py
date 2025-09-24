468242be4573eaed3c42b8bb10b25dfd6e45f067
William Sullivan <willwill.sullivan@gmail.com>
2025-09-24 01:38:18 -0500

    fix error msg

--- heathcliff.py
+++ heathcliff.py
@@ -5,7 +5,7 @@ class heathcliff:
     async def run(self, message):
         if (message.content == "!heathcliff"):
             ch = comics.search("heathcliff", dt.datetime.now().date())
-            if ch:
+            try:
                 await message.channel.send(ch.image_url)
-            else:
+            except comics.exceptions.InvalidDateError:
                 await message.channel.send("hasn't been uploaded yet, wait a few hours pls")
