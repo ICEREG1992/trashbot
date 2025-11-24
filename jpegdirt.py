79627dcffeeba4c77481546db7bffa1eb996e550
William Sullivan <willwill.sullivan@gmail.com>
2025-11-24 01:25:06 -0600

    try weird regex thing

--- jpegdirt.py
+++ jpegdirt.py
@@ -1,8 +1,9 @@
 import requests
+import re
 
 class jpegdirt:
     async def run(self, message):
         parts = message.content.split(' ', 1)
-        if parts[0] == "jpegdirt!random":
+        if re.match(r'jp.*gdirt!random', parts[0]):
             r = requests.get("https://jpegdirt.tumblr.com/random", allow_redirects=True)
             await message.channel.send(r.url)
