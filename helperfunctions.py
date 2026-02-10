127e1f067d241b3d931bfac370900bd6e18ec0f6
William Sullivan <willwill.sullivan@gmail.com>
2026-02-09 23:26:36 -0600

    switch to raw float

--- helperfunctions.py
+++ helperfunctions.py
@@ -92,7 +92,7 @@ def ensure_table():
 def fill_defaults(db):
     # fill empty required values with defaults
     keynames = ["contains_phrases", "equals_phrases", "regex_phrases", "hunger", "lyrics", "mc_ip", "permissions", "uptime", "todo", "wordplay_keywords", "on", "lipo", "pan", "reminders", "quests", "boots"]
-    defaults = ["{}", "{}", "{}", "{}", "•", "", "{}", "0", "{}", "{}", "True", "{}", "{}", "[]", "{}", "{}"]
+    defaults = ["{}", "{}", "{}", "{}", "•", "", "{}", "0", "{}", "{}", "True", "{}", "{}", "[]", "{}", "0"]
     for i in range(len(keynames)):
         n = db.get_item(TableName="trashbot", Key={'name':{'S':keynames[i]}})
         if ('Item' not in n):
