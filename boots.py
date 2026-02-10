013f6d3546822698d641786249bf9f7fe88ed211
William Sullivan <willwill.sullivan@gmail.com>
2026-02-09 23:34:19 -0600

    fix save

--- boots.py
+++ boots.py
@@ -18,7 +18,7 @@ class boots:
             data = json.loads(d['Item']['data']['S'])
             if data == "0":
                 t = dt.datetime.now(dt.timezone.utc)
-                boots.save()
+                boots.save(t)
             else:
                 t = dt.datetime.fromtimestamp(float(data), tz=dt.timezone.utc)
 
@@ -28,8 +28,7 @@ class boots:
             new_t = dt.datetime.now(dt.timezone.utc)
             await message.channel.send("Days since last swampy boots: ~~" + str((new_t.date() - t.date()).days) + "~~ 0")
             t = new_t
-            boots.save()
+            boots.save(t)
 
-    def save():
-        global t
+    def save(t):
         db.put_item(TableName="trashbot", Item={'name':{'S':'boots'}, 'data':{'S':json.dumps(t.timestamp())}})
